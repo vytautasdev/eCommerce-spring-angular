824c9c671bafa090270f39105ecc41ae152ce723
vytautasdev <vytautasdev@gmail.com>
2022-08-18 22:52:38 +0100

    stripe-api-impl-backend-commit

--- backend/src/main/java/com/vytautasdev/ecommerce/controller/CheckoutController.java
+++ backend/src/main/java/com/vytautasdev/ecommerce/controller/CheckoutController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
--- backend/src/main/java/com/vytautasdev/ecommerce/service/CheckoutServiceImpl.java
+++ backend/src/main/java/com/vytautasdev/ecommerce/service/CheckoutServiceImpl.java
@@ -79,6 +79,8 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "luv2shop purchase");
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
 
         return PaymentIntent.create(params);
     }
